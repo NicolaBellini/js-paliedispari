fc17d41ae84824e9bd58d0ec0e6111c5472f8852
NicolaBellini <bellininicola4366@gmail.com>
2024-02-29 11:04:16 +0100

    [fix] the code

--- pari o dispari/JS/script.js
+++ pari o dispari/JS/script.js
@@ -1,115 +1,53 @@
 
 
+const giocata = prompt("scegli pari o dipsari")
+const numeroGiocatore =parseInt (prompt("scegli un numero "))
+const numPc = getRandomNumber()
 
+// qui sommo e lo passo alla funzione
+const somma = numPc + numeroGiocatore
 
+ let risultato;
 
-
- // elements
- const random = getRandomNum(1, 5).toFixed(0);
- const numPc = random;
- let numUser = "";
-
- // flag che mi consente di uscire da loop while
-  let validNumber = false;
-  let evenNumber = false
- // attribuisco il risultato della mia funzione ad una costante eliminando tutti i numeri dopo la virgola con il tofixed
-
-
-console.log(numPc);
-
-// // while per verificare che nel prompt venga messo un numero valido
- while(!validNumber){
-  // chiedo all utente di immettere un numero da 1 a 5 con un prompt e ripeto l operazione se l utente inserisce un numero non valido
-  numUser = prompt("inserire un numero da 1 a 5");
-// controllo per vedere se il numero è valido
-   if(numUser<=5 && numUser>0){
-   console.log("il numero è valido");
-   validNumber = true;
-   }else{
-     validNumber = false;
-    console.log(" il numero non è compreso tra 1 e 5");
-   };
-
-} 
-
- const oddOrEven = prompt("scegli pari o dispari")
-
- let even = ""
-
- if(oddOrEven === "pari"){
-   // pari
-   even = true
+ if(giocata === "pari" && isEven(somma)  || giocata === "dispari" && !isEven(somma)){
+  console.log(getRisultato() +" vince il giocatore");
+  risultato = getRisultato() +"vince il giocatore"
  }else{
-   // dispari
-   even = false
+   console.log(getRisultato() +" vince il pc")
+   risultato = getRisultato() +"vince il pc"
  }
 
- console.log(numUser)
 
- // funzione per capire se la somma è pari
- comparer(numPc, parseInt(numUser));
+// ternario
 
+// const risultato = (giocata === "pari" && isEven(somma) || giocata === "dispari" && !isEven(somma))
+// ? getRisultato() + "vince il giocatore"
+// : getRisultato() + "vince il pc"
 
- // blocco if per stampare il risultato del comparer
- if(evenNumber){
-   console.log("la somma è pari");
- }else{
-   console.log("la somma è dispari");
- }
-
- console.log(evenNumber, oddOrEven, numUser);
- oddEven(evenNumber, oddOrEven)
+console.log(numPc, numeroGiocatore, somma, isEven(somma))
 
 
- // _________FUNCTION_________
 
- // creo la funzione per il numero casuale
- function getRandomNum(min, max){
-   return Math.random() * (max- min) + min;
- };
+// funzioni//////////
 
- // creo la funzione che somma e compara le cifre
- function comparer(numA, numB){
-   const sum = numA + numB
+// random numero
+function getRandomNumber(){
+  return Math.ceil(Math.random() * 5)
+}
 
-   if(sum % 2===0){
- //  pari
-     evenNumber = true
-   }else {
-     // somma è dispari
-     evenNumber = false
-   }
-  
- }
+// per capire se la somma è pari
+function isEven(number){
+  if(number % 2 === 0){
+    return true
+  }
+  return false
+}
 
- function oddEven(even, evenNumber){
- // se la somma è pari
-   if((even && evenNumber) || (!even && !evenNumber)){
-     console.log("il giocatore ha vinto")
-   }else{
-     console.log("il pc ha vinto")
-   }
- }
- //se il giocatore ha messo pari e la somma è pari vince
- function winner(somma, scelta){
-   if(((scelta === "pari") && (somma = true)) || ((scelta === "dispari") && (somma = false))){
-     console.log("il giocatoe ha vinto")
-   }else{
-     console.log("il pc ha vinto")
-   }
-
+function getRisultato(){
+  return `numm giocatore: ${numeroGiocatore}, giocata ${giocata}, num pc ${numPc}`
 }
 
 
 
 
-// // secondo tentativo
 
-// function oddEven(even, evenNumber){
-//   // se la somma è pari
-//     if((evenNumber && (evenNumber === even)) || (!evenNumber && (evenNumber === !even))){
-//       console.log("il giocatore ha vinto")
-//     }else if((evenNumber && (evenNumber === !even)) || (!evenNumber && (evenNumber === even))){
-//       console.log("il pc ha vinto")
-//     }
-//   }
--- pari o dispari/README.md
+++ pari o dispari/README.md
@@ -7,9 +7,20 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 
-chiedo all utente con un prompt di nserire un numero da 1 a 5 mettendo un controllo di validità
+<!-- chiedo all utente con un prompt di nserire un numero da 1 a 5 mettendo un controllo di validità
 
 creo una funzione che genera un numero random e ne assegnerà il risultato a una costante, la quale sarà il numero del pc.
 
 
-con una funzione, sommo i due numeri e dopo faccio %2 della somma per vedere se ho ottenuto un risultato pari o dispari  
+con una funzione, sommo i due numeri e dopo faccio %2 della somma per vedere se ho ottenuto un risultato pari o dispari   -->
+
+stefano 
+
+-salvo in una costante il prompt con pari dispari
+-salvo in una costante il prompt con numero scelto 
+-creo una funzione che restituisca un numero random da 1 a 5 e lo salvo in una variabile
+-sommo i due numeri
+-creo una funzione che mi restituisca un valore booleano se pari o dispari e la utilizzo per l output finale 
+
+
+
